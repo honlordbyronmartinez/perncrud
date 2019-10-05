86c56ebd8a53ae9dbbe338a458180c4f6af37c81
Byron Martinez <mr.byronmartinez@gmail.com>
2019-10-04 18:57:59 -0700

    modular

---
+++ crudpractice-frontend/src/modules/crm/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const crm = () => (
+    <div>CRM Module</div>
+);
+
+export default {
+    routeProps: {
+        path: '/crm',
+        component: CRM
+    },
+    name: 'CRM',
+}
---
+++ crudpractice-frontend/src/modules/projects/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const projects = () => (
+    <div>Projects Module</div>
+);
+
+export default {
+    routeProps: {
+        path: '/projects',
+        component: Projects
+    },
+    name: 'Projects',
+}
